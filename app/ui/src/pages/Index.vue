f6c54b54ce7f2b8cbe6c658b4dd180c1f6d4dd31
Peter Peterson <peter.peterson.iv@gmail.com>
2020-06-16 08:24:31 -0700

    Using quasar vue

---
+++ app/ui/src/pages/Index.vue
@@ -0,0 +1,30 @@
+<template>
+  <q-page class="flex flex-center">
+    <q-card class="my-card">
+      <q-card-section>
+        {{ sup }}
+      </q-card-section>
+    </q-card>
+  </q-page>
+</template>
+
+<script>
+import {
+  QCard,
+  QCardSection
+} from 'quasar'
+
+export default {
+    name: 'PageIndex',
+    components: {
+      QCard,
+      QCardSection
+    },
+    data (){
+      return {
+        sup: "Sup yo"
+      }
+    }
+
+}
+</script>
