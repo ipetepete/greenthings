a323154994fea1658bfc412043c54d69210028cf
Peter P <peter.peterson.iv@gmail.com>
2020-06-16 10:41:14 -0700

    Included axios for ajax requests

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 import gpiozero as gp
 app = Flask(__name__)
 
+CORS(app)
 
 @app.route('/get_state')
 def get_state():
--- app/ui/src/pages/Index.vue
+++ app/ui/src/pages/Index.vue
@@ -3,6 +3,10 @@
     <q-card class="my-card">
       <q-card-section>
         {{ sup }}
+        <q-card-actions>
+          <q-btn flat @click="foobar">Action 1</q-btn>
+          <q-btn flat>Action 2</q-btn>
+        </q-card-actions>
       </q-card-section>
     </q-card>
   </q-page>
@@ -11,14 +15,27 @@
 <script>
 import {
   QCard,
-  QCardSection
+  QCardSection,
+  QCardActions,
 } from 'quasar'
 
 export default {
     name: 'PageIndex',
     components: {
       QCard,
-      QCardSection
+      QCardSection,
+      QCardActions
+    },
+    methods: {
+      foobar: function(){
+        this.$axios.get("http://localhost:5000/get_state")
+            .then(function(data){
+              console.log("got this", data);
+            })
+            .catch(function(err){
+              console.error(err);
+            });
+      }
     },
     data (){
       return {
