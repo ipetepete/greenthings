27ed704d305dfb4422b6d19aa2a14218cc2aa974
Peter Peterson <peter.peterson.iv@gmail.com>
2020-06-16 08:25:12 -0700

    Missed some files

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
-from flask import Flask
-from RPi import GPIO 
+from flask import Flask, jsonify
+import gpiozero as gp
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+
+@app.route('/get_state')
+def get_state():
+    return jsonify({'status':'on'})
